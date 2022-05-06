3a197645471e47b8d453be7760d9736b76c89c37
The-Robin-Zone <shakedrobinzon@gmail.com>
2022-05-06 12:01:47 +0300

    Initial check-in of module B22Ex02ShakedRobinzon203943253FannyGuthmann337957633

---
+++ B22Ex02ShakedRobinzon203943253FannyGuthmann337957633/Coin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace B22Ex02ShakedRobinzon203943253FannyGuthmann337957633
+{
+    public class main
+    {
+        public static void Main()
+        {
+            Console.WriteLine("shaked");
+            Console.ReadLine();
+        }
+    }
+}
