bb17b21d2cbb79a73b5f00e0a862f2a8b458df8a
fannyguthmann <fanny.guthmann@post.idc.ac.il>
2022-05-06 13:32:52 +0300

    hhh

--- B22Ex02ShakedRobinzon203943253FannyGuthmann337957633/Coin.cs
+++ B22Ex02ShakedRobinzon203943253FannyGuthmann337957633/Coin.cs
@@ -3,16 +3,55 @@ namespace B22Ex02ShakedRobinzon203943253FannyGuthmann337957633
 {
     public class Coin
     {
-        public char coinColor;  //  black/white (O/X)
-        public string coinType; //  pawn/queen
-        public string location; // location on board
+        private char coinColor;  //  black/white (O/X)
+        private string coinType; //  pawn/queen
+        private string location; // location on board
 
         // Coin object constructor 
         public Coin(char i_coinColor, string i_coinType)
         {
-            coinColor = i_coinColor;
-            coinType = i_coinType;
-            location = null;
+            this.coinColor = i_coinColor;
+            this.coinType = i_coinType;
+            this.location = null;
+        }
+
+        public char CoinColor
+        {
+            set
+            {
+                this.coinColor = value;
+            }
+            get
+
+            {
+                return this.coinColor;
+            }
+        }
+
+        public string CoinType
+        {
+            set
+            {
+                this.coinType = value;
+            }
+            get
+
+            {
+                return this.coinType;
+            }
+        }
+
+        public string Location
+        {
+            set
+            {
+                this.location = value;
+            }
+            get
+
+            {
+                return this.location;
+            }
         }
     }
 }
