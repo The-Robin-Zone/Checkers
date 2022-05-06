6ca6ef8bc4e1b6b6887a47649481e45fcb1418c7
shakedrobinzon <shakedrobinzon@gmail.com>
2022-05-06 13:03:32 +0300

    prog update

--- B22Ex02ShakedRobinzon203943253FannyGuthmann337957633/Program.cs
+++ B22Ex02ShakedRobinzon203943253FannyGuthmann337957633/Program.cs
@@ -5,7 +5,22 @@ namespace B22Ex02ShakedRobinzon203943253FannyGuthmann337957633
     {
         public static void Main()
         {
-            Console.WriteLine("shakedRobinzon2022");
+            initializeGame();
+        }
+
+    public static void initializeGame()
+        {
+            Console.WriteLine("Welcome to Checkers!");
+            Console.WriteLine("Please choose number of players: 1/2");
+            int numberOfPlayers = input.numberOfPlayers;
+            if ( numberOfPlayers == 2)
+            {
+                Console.WriteLine("Not supported yet");
+                initializeGame();
+            }
+            
+            input.enterName();
+
         }
     }
 }
