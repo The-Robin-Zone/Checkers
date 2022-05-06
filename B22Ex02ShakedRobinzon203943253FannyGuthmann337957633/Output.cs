80af414a389904ecfab0ca6d9adb648b19055a2c
shakedrobinzon <shakedrobinzon@gmail.com>
2022-05-06 14:58:01 +0300

    1

---
+++ B22Ex02ShakedRobinzon203943253FannyGuthmann337957633/Output.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace B22Ex02ShakedRobinzon203943253FannyGuthmann337957633
+{
+    public class Output
+    {
+        public Output()
+        {
+
+        }
+        public static void Print2DArray(char[,] boardGame)
+        {
+            for (int i = 1; i < boardGame.Length - 3; i++)
+            {
+                for (int j = 1; j < boardGame.Length - 3; j++)
+                {
+                    Console.WriteLine("shaked");
+                }
+            }
+        }
+    }
+}
--- B22Ex02ShakedRobinzon203943253FannyGuthmann337957633/Program.cs
+++ B22Ex02ShakedRobinzon203943253FannyGuthmann337957633/Program.cs
@@ -5,7 +5,12 @@ namespace B22Ex02ShakedRobinzon203943253FannyGuthmann337957633
     {
         public static void Main()
         {
-            initializeGame();
+            //initializeGame();
+            char[,] board = initializeBoard(10);
+            Output.Print2DArray(board);
+            Console.WriteLine("shaked");
+
+
         }
 
     public static void initializeGame()
@@ -14,15 +19,81 @@ namespace B22Ex02ShakedRobinzon203943253FannyGuthmann337957633
             string namePlayer1 = Input.getPlayerName();
             Player player1 = new Player(namePlayer1);
             string namePlayer2 = Input.getPlayerName();
-           
+
             Player player2 = new Player(namePlayer1);
             int boardSize = Input.boardSize();
             //initializeBoard();
+
+
         }
 
-    //public static void initializeBoard()
-    //    {
-    //        Coin[,] gameBoard = new Coin[,];
-    //    }
+        public static char[,] initializeBoard(int i_boardSize)
+        {
+
+            // add 2 rows and 2 colums for padding
+            int numOfCoinRows = (i_boardSize / 2) - 1;
+            i_boardSize = i_boardSize + 3;
+
+            char[,] gameBoard = new char[i_boardSize, i_boardSize];
+
+            // initialize O Coins
+            for (int i = 1; i < numOfCoinRows; i++)
+            {
+                for (int j = 1; j < i_boardSize - 1; j++)
+                {
+                    // odd row
+                    if (i % 2 != 0)
+                    {
+                        // even column
+                        if ( j % 2 == 0)
+                        {
+                            gameBoard[i, j] = 'O';
+                        }
+                        
+                    }
+
+                    // even row
+                    if (i % 2 == 0)
+                    {
+                        // odd column
+                        if ( j % 2 != 0)
+                        {
+                            gameBoard[i, j] = 'O';
+                        }
+                    }
+
+                }
+            }
+            // initialize X Coins
+            for (int i = i_boardSize - 2; i > numOfCoinRows + 2; i--)
+            {
+                for (int j = 1; j < i_boardSize - 1; j++)
+                {
+                    // odd row
+                    if (i % 2 != 0)
+                    {
+                        // even column
+                        if (j % 2 == 0)
+                        {
+                            gameBoard[i, j] = 'X';
+                        }
+
+                    }
+
+                    // even row
+                    if (i % 2 == 0)
+                    {
+                        // odd column
+                        if (j % 2 != 0)
+                        {
+                            gameBoard[i, j] = 'X';
+                        }
+                    }
+
+                }
+            }
+
+            return gameBoard;
+        }
     }
 }
