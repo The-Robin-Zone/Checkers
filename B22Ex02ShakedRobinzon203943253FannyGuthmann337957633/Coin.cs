f6a222bcf9267487481ebbe35362edf05bbadb50
shakedrobinzon <shakedrobinzon@gmail.com>
2022-05-06 12:28:29 +0300

    * Coin.cs: * Player.cs:
    * B22Ex02ShakedRobinzon203943253FannyGuthmann337957633.csproj: coin
      and player objects

--- B22Ex02ShakedRobinzon203943253FannyGuthmann337957633/Coin.cs
+++ B22Ex02ShakedRobinzon203943253FannyGuthmann337957633/Coin.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 namespace B22Ex02ShakedRobinzon203943253FannyGuthmann337957633
 {
-    public class main
+    public class Coin
     {
-        public static void Main()
+        public char coinColor;  //  black/white (O/X)
+        public string coinType; //  pawn/queen
+        public string location; // location on board
+
+        // Coin object constructor 
+        public Coin(char i_coinColor, string i_coinType)
         {
-            Console.WriteLine("shaked");
-            Console.WriteLine("robin");
-            Console.ReadLine();
+            coinColor = i_coinColor;
+            coinType = i_coinType;
+            location = null;
         }
     }
 }
---
+++ B22Ex02ShakedRobinzon203943253FannyGuthmann337957633/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace B22Ex02ShakedRobinzon203943253FannyGuthmann337957633
+{
+    public class Player
+    {
+        public string playerName;
+        public int score;
+
+        // Player object constructor 
+        public Player(string i_playerName)
+        {
+            playerName = i_playerName;
+            score = 0;
+            
+        }
+    }
+}
