8a6e36f57ab9f05ddc962a90a04fee3eb58fa77e
shakedrobinzon <shakedrobinzon@gmail.com>
2022-05-06 17:23:23 +0300

    * Program.cs:
    * Output.cs: 1

--- B22Ex02ShakedRobinzon203943253FannyGuthmann337957633/Output.cs
+++ B22Ex02ShakedRobinzon203943253FannyGuthmann337957633/Output.cs
@@ -7,32 +7,53 @@ namespace B22Ex02ShakedRobinzon203943253FannyGuthmann337957633
         {
 
         }
+
         public static void Print2DArray(char[,] boardGame, int arrayDimention)
         {
-            //Console.WriteLine(boardGame.Length);
-            //Console.WriteLine(boardGame[1, 2]);
-            //Console.WriteLine(boardGame[1, 4]);
-            //Console.WriteLine(boardGame[1, 6]);
+            
+            char columnPrint = 'A';
+            char rowPrint = 'a';
+            // Print Column letters
+            Console.Write("  ");
+            for (int i = 0; i < arrayDimention; i++)
+            {
+
+                Console.Write(" " + (char)(columnPrint+i) + "  ");
+            }
+
+        Console.WriteLine("");
+        PrintRowSeperator(arrayDimention);
 
             for (int i = 1; i < arrayDimention + 1; i++)
             {
-                //Console.WriteLine("i index is: " + i);
+            Console.Write((char)(rowPrint + i - 1));
                 for (int j = 1; j < arrayDimention +1; j++)
                 {
-                    //Console.WriteLine("j index is: " + j);
                     if (boardGame[i, j] != 'O' && boardGame[i, j] != 'X')
                     {
-                        Console.Write("-");
+                        Console.Write("|   ");
                     }
                     else
                     {
-                        Console.Write(boardGame[i,j]);
+                        Console.Write("| " + boardGame[i,j] + " ");
                     }
                     
                
                 }
-                Console.WriteLine();
+                Console.WriteLine("|");
+                PrintRowSeperator(arrayDimention);
             }
+            
+        }
+        public static void PrintRowSeperator(int boardSize)
+        {
+            Console.Write(" ");
+            for (int i = 0; i < (boardSize * 4); i++)
+            {
+                Console.Write("=");
+                
+            }
+            Console.WriteLine("");
         }
     }
 }
--- B22Ex02ShakedRobinzon203943253FannyGuthmann337957633/Program.cs
+++ B22Ex02ShakedRobinzon203943253FannyGuthmann337957633/Program.cs
@@ -6,8 +6,8 @@ namespace B22Ex02ShakedRobinzon203943253FannyGuthmann337957633
         public static void Main()
         {
             //initializeGame();
-            char[,] board = initializeBoard(10);
-            Output.Print2DArray(board,10);
+            Coin[,] gameBoard = initializeBoard(10);
+            Output.Print2DArray(gameBoard,10);
             Console.ReadLine();
             
 
@@ -19,11 +19,12 @@ namespace B22Ex02ShakedRobinzon203943253FannyGuthmann337957633
         {
             int numberOfPlayers = Input.numberOfPlayers();
             string namePlayer1 = Input.getPlayerName();
-            Player player1 = new Player(namePlayer1);
             string namePlayer2 = Input.getPlayerName();
-
-            Player player2 = new Player(namePlayer1);
+            
             int boardSize = Input.boardSize();
+            Player player1 = new Player(namePlayer1, boardSize);
+
+            Player player2 = new Player(namePlayer2, boardSize);
             //initializeBoard();
 
 
@@ -31,17 +32,11 @@ namespace B22Ex02ShakedRobinzon203943253FannyGuthmann337957633
 
         public static char[,] initializeBoard(int i_boardSize)
         {
-
             int numOfCoinRows = (i_boardSize / 2) - 1;
             // add 2 rows and 2 colums for padding
             i_boardSize = i_boardSize + 2;
 
             char[,] gameBoard = new char[i_boardSize, i_boardSize];
-
-
-            //Console.WriteLine("actual board size is: " + i_boardSize);
-            //Console.WriteLine("num of coing row is: " + numOfCoinRows);
-
 
             //initialize O Coins
             for (int i = 1; i < numOfCoinRows + 1; i++)
