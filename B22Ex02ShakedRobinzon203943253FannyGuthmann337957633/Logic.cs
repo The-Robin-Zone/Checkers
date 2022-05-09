73b2e1e61983105c5bd8e3c774e4e3576652e750
fannyguthmann <fanny.guthmann@post.idc.ac.il>
2022-05-09 17:17:57 +0300

    Debug logic and added king option

--- B22Ex02ShakedRobinzon203943253FannyGuthmann337957633/Logic.cs
+++ B22Ex02ShakedRobinzon203943253FannyGuthmann337957633/Logic.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using System.Collections;
 namespace B22Ex02ShakedRobinzon203943253FannyGuthmann337957633
 {
     public class Logic
@@ -23,6 +24,7 @@ namespace B22Ex02ShakedRobinzon203943253FannyGuthmann337957633
             {
                 o_typeMove = false;
             }
+            // Check if the tile is occupy by the right coin
             else if (!CoinExistAtLocation(gameBoard, xStart, yStart, player.Color))
             {
                 o_typeMove = false;
@@ -37,7 +39,7 @@ namespace B22Ex02ShakedRobinzon203943253FannyGuthmann337957633
                 // Player move is not check
                 o_typeMove = false;
                 // Check that player is doing a simple move
-                if (IsSimpleMove(player.Color, xStart, yStart, xEnd, yEnd))
+                if (IsSimpleMove(gameBoard, player.Color, xStart, yStart, xEnd, yEnd))
                 {
                     o_typeMove = true;
                     if (!NoOpponentToEat(gameBoard, player.Color))
@@ -59,6 +61,13 @@ namespace B22Ex02ShakedRobinzon203943253FannyGuthmann337957633
             int xPoint, int yPoint, char playerColor)
         {
             bool o_coinExistAtLocation = true;
+            if (playerColor.CompareTo('O') == 0 && gameBoard.Board[xPoint, yPoint].IsKing)
+            {
+                playerColor = 'Q';
+            } else if (playerColor.CompareTo('X') == 0 && gameBoard.Board[xPoint, yPoint].IsKing)
+            {
+                playerColor = 'Z';
+            }
 
             if (gameBoard.Board[xPoint, yPoint].CoinColor.CompareTo(playerColor) != 0)
             {
@@ -81,21 +90,37 @@ namespace B22Ex02ShakedRobinzon203943253FannyGuthmann337957633
             return o_coinDestinationIsFree;
         }
 
+        // Check if the tile is occupied by the given color
+        private static bool tileOccupiedByColor(GameBoard gameBoard, int xPoint, int yPoint,
+            char playerColor)
+        {
+            bool tileIsnotEmptyAndOccupyByRightColor = true;
+            if (IsTileFree(gameBoard, xPoint, yPoint))
+            {
+                tileIsnotEmptyAndOccupyByRightColor = false;
+            }
+            else if (!CoinExistAtLocation(gameBoard, xPoint, yPoint, playerColor))
+            {
+                tileIsnotEmptyAndOccupyByRightColor = false;
+            }
+            return tileIsnotEmptyAndOccupyByRightColor;
+        }
+
         // Check that the move is a simple legal move
-        public static bool IsSimpleMove(char playerColor, int xStart,
+        public static bool IsSimpleMove(GameBoard gameBoard, char playerColor, int xStart,
             int yStart, int xEnd, int yEnd)
         {
             bool isSimpleMove = true;
             if (Math.Abs(xEnd - xStart) == 1 && Math.Abs(yEnd - yStart) == 1)
             {
-                if (playerColor.CompareTo('O') == 0)
+                if (playerColor.CompareTo('O') == 0 && !gameBoard.Board[xStart, yStart].IsKing)
                 {
                     if (xEnd < xStart)
                     {
                         isSimpleMove = false;
                     }
                 }
-                else if (playerColor.CompareTo('X') == 0)
+                else if (playerColor.CompareTo('X') == 0 && !gameBoard.Board[xStart, yStart].IsKing)
                 {
                     if (xEnd > xStart)
                     {
@@ -119,12 +144,17 @@ namespace B22Ex02ShakedRobinzon203943253FannyGuthmann337957633
             {
                 int xMidlle = (xStart + xEnd) / 2;
                 int yMidlle = (yStart + yEnd) / 2;
-                if (playerColor.CompareTo('O') == 0)
+                if (!MoveIsInbound(gameBoard, xStart, yStart, xEnd, yEnd))
                 {
-                    if (xEnd < xStart)
+                    isJump = false;
+                }
+                else if (playerColor.CompareTo('O') == 0)
+                {
+                    if (xEnd < xStart && !gameBoard.Board[xStart, yStart].IsKing)
                     {
                         isJump = false;
-                    } else if (IsTileFree(gameBoard, xMidlle, yMidlle) ||
+                    } else if (!IsTileFree(gameBoard, xEnd, yEnd)
+                        || IsTileFree(gameBoard, xMidlle, yMidlle) ||
                         CoinExistAtLocation(gameBoard, xMidlle, yMidlle, playerColor))
                     {
                         isJump = false;
@@ -132,16 +162,16 @@ namespace B22Ex02ShakedRobinzon203943253FannyGuthmann337957633
                 }
                 else if (playerColor.CompareTo('X') == 0)
                 {
-                    if (xEnd > xStart)
+                    if (xEnd > xStart && !gameBoard.Board[xStart, yStart].IsKing)
                     {
                         isJump = false;
-                    } else if (IsTileFree(gameBoard, xMidlle, yMidlle) ||
+                    } else if (!IsTileFree(gameBoard, xEnd, yEnd)
+                        || IsTileFree(gameBoard, xMidlle, yMidlle) ||
                         CoinExistAtLocation(gameBoard, xMidlle, yMidlle, playerColor))
                     {
                         isJump = false;
                     }
-                }
-                
+                } 
             }
             else
             {
@@ -164,21 +194,24 @@ namespace B22Ex02ShakedRobinzon203943253FannyGuthmann337957633
             {
                 for (int j = 1; j < gameBoard.BoardSize - 1; j++)
                 {
-                    if (tileOccupiedByRightColor(gameBoard, i, j, opponentColor))
+                    if (tileOccupiedByColor(gameBoard, i, j, playerColor))
                     {
 
                         // Check if element need to go up or down
                         int xPoint = i + 1;
                         int xEndJump = i + 2;
+                        bool isX = false;
                         if (playerColor.CompareTo('X') == 0)
                         {
                             xPoint = i - 1;
                             xEndJump = i - 2;
+                            isX = true;
                         }
-                        
+
                         // Check if can eat one of his neighbor going to the right side
-                        if (tileOccupiedByRightColor(gameBoard, xPoint, j + 1, playerColor) &&
-                            !coinIsAtBorderOfBoard(gameBoard, xPoint, j + 1))
+                        if (tileOccupiedByColor(gameBoard, xPoint, j + 1, opponentColor) &&
+                            !coinIsAtBorderOfBoard(gameBoard, xPoint, j + 1) &&
+                             IsTileFree(gameBoard, xEndJump, j + 2))
                         {
                             if (IsJump(gameBoard, playerColor, i, j, xEndJump, j + 2))
                             {
@@ -188,8 +221,9 @@ namespace B22Ex02ShakedRobinzon203943253FannyGuthmann337957633
 
                         }
                         // Check if can eat one of his neighbor going to the left side
-                        if (tileOccupiedByRightColor(gameBoard, xPoint, j - 1, playerColor) &&
-                          !coinIsAtBorderOfBoard(gameBoard, xPoint, j - 1))
+                        if (tileOccupiedByColor(gameBoard, xPoint, j - 1, opponentColor) &&
+                          !coinIsAtBorderOfBoard(gameBoard, xPoint, j - 1) &&
+                          IsTileFree(gameBoard, xEndJump, j - 2))
                         {
                             if (IsJump(gameBoard, playerColor, i, j, xEndJump, j - 2))
                             {
@@ -203,21 +237,6 @@ namespace B22Ex02ShakedRobinzon203943253FannyGuthmann337957633
             }
         end:
             return noOpponentToEat;
-        }
-
-        // Check if the tile is occupied by the given color
-        private static bool tileOccupiedByRightColor(GameBoard gameBoard, int xPoint, int yPoint, char playerColor)
-        {
-            bool tileIsnotEmptyAndOccupyByRightColor = true;
-            if (IsTileFree(gameBoard, xPoint, yPoint))
-            {
-                tileIsnotEmptyAndOccupyByRightColor = false;
-            }
-            else if (CoinExistAtLocation(gameBoard, xPoint, yPoint, playerColor))
-            {
-                tileIsnotEmptyAndOccupyByRightColor = false;
-            }
-            return tileIsnotEmptyAndOccupyByRightColor;
         }
 
         // Check if the coin is at one of the extremity of the board
@@ -238,19 +257,19 @@ namespace B22Ex02ShakedRobinzon203943253FannyGuthmann337957633
         {
             bool moveIsInBound = true;
 
-            if (xStart > gameBoard.BoardSize - 2)
+            if (xStart > gameBoard.BoardSize - 2 || xStart <= 0)
             {
                 moveIsInBound = false;
             }
-            else if (yStart > gameBoard.BoardSize - 2)
+            else if (yStart > gameBoard.BoardSize - 2 || yStart <= 0)
             {
                 moveIsInBound = false;
             }
-            else if (xEnd > gameBoard.BoardSize - 2)
+            else if (xEnd > gameBoard.BoardSize - 2 || xEnd <= 0)
             {
                 moveIsInBound = false;
             }
-            else if (yEnd > gameBoard.BoardSize - 2)
+            else if (yEnd > gameBoard.BoardSize - 2 || yEnd <= 0)
             {
                 moveIsInBound = false;
             }
@@ -259,7 +278,8 @@ namespace B22Ex02ShakedRobinzon203943253FannyGuthmann337957633
         }
 
         // Check if another jump is possible
-        public static bool IsJumpAvalaible(GameBoard gameBoard, char playerColor, int xPoint, int yPoint)
+        public static bool IsJumpAvalaible(GameBoard gameBoard, char playerColor,
+            int xPoint, int yPoint)
         {
             bool o_isJumpAvailable = false;
             // Check if the coin is a king
@@ -273,18 +293,39 @@ namespace B22Ex02ShakedRobinzon203943253FannyGuthmann337957633
             // If the element is a king enter the two if and check 4 options if not check the only two option available for it
             if (isKing || isX)
             {
-                if (IsJump(gameBoard, playerColor, xPoint, yPoint, xPoint - 2, yPoint - 2)
-                    || IsJump(gameBoard, playerColor, xPoint, yPoint, xPoint - 2, yPoint + 2))
-                {
-                    o_isJumpAvailable = true;
+                if (MoveIsInbound(gameBoard, xPoint, yPoint, xPoint - 2, yPoint - 2))
+                { 
+                    if ( IsJump(gameBoard, playerColor, xPoint, yPoint, xPoint - 2, yPoint - 2))
+                    {
+                        o_isJumpAvailable = true;
+                    }
                 }
+                if (MoveIsInbound(gameBoard, xPoint, yPoint, xPoint - 2, yPoint + 2))
+                {
+                    if (IsJump(gameBoard, playerColor, xPoint, yPoint, xPoint - 2, yPoint + 2))
+                    {
+                        o_isJumpAvailable = true;
+
+                    }
+                }
+                
             }
             if (isKing || !isX)
             {
-                if (IsJump(gameBoard, playerColor, xPoint, yPoint, xPoint + 2, yPoint - 2)
-                    || IsJump(gameBoard, playerColor, xPoint, yPoint, xPoint + 2, yPoint + 2))
+                if (MoveIsInbound(gameBoard, xPoint, yPoint, xPoint + 2, yPoint - 2))
                 {
-                    o_isJumpAvailable = true;
+                    if (IsJump(gameBoard, playerColor, xPoint, yPoint, xPoint + 2, yPoint - 2))
+                    {
+                        o_isJumpAvailable = true;
+                    }
+                }
+                if (MoveIsInbound(gameBoard, xPoint, yPoint, xPoint + 2, yPoint + 2))
+                {
+                    if (IsJump(gameBoard, playerColor, xPoint, yPoint, xPoint + 2, yPoint + 2))
+                    {
+                        o_isJumpAvailable = true;
+
+                    }
                 }
             }
             return o_isJumpAvailable;
@@ -316,6 +357,132 @@ namespace B22Ex02ShakedRobinzon203943253FannyGuthmann337957633
             }
             return o_shouldTurnKing;
         }
+
+        //Computer part
+
+        public static int[] NextMoveComputer(GameBoard gameBoard, Player player)
+        {
+            int[] o_nextMove = new int[2];
+            Random random = new Random();
+            ArrayList allMovePossible = AllMovePossible(gameBoard, player);
+            // Do the random selection
+            int numberMovePossible = allMovePossible.Count;
+            int indexMove = random.Next(numberMovePossible);
+            o_nextMove = (int[])allMovePossible[indexMove];
+            return o_nextMove;
+        }
+
+        // if capacity is equal to zero it means we dont have any element in the array
+        //Console.WriteLine(allMovePossible.Capacity);
+        public static ArrayList AllMovePossible(GameBoard gameBoard, Player player)
+        {
+            ArrayList o_allMovePossible = new ArrayList(new ArrayList());
+            bool isJumpPossible = NoOpponentToEat(gameBoard, player.Color);
+            if (isJumpPossible)
+            {
+                o_allMovePossible = allJumpAvailable(gameBoard, player);
+            } else
+            {
+                o_allMovePossible = allSimpleMoveAvailable(gameBoard, player);
+            }         
+            return o_allMovePossible;
+        }
+
+        private static ArrayList allJumpAvailable(GameBoard gameBoard, Player player)
+        {
+            ArrayList o_allJumpAvailable = new ArrayList();
+
+            for (int i = 1; i < gameBoard.BoardSize - 1; i++)
+            {
+                for (int j = 1; j < gameBoard.BoardSize - 1; j++)
+                {
+                    if (tileOccupiedByColor(gameBoard, i, j, player.Color))
+                    {
+                        bool isKing = gameBoard.Board[i, j].IsKing;
+                       
+                        if (isKing || (gameBoard.Board[i, j].CoinColor.CompareTo('X') == 0))
+                        {
+                            if (MoveIsInbound(gameBoard, i, j, i - 2, j - 2) &&
+                                IsJump(gameBoard, player.Color, i, j, i - 2, j - 2))
+                            {
+                                o_allJumpAvailable.Add(new ArrayList() { i - 2, j - 2 });
+                            }
+                              if (MoveIsInbound(gameBoard, i, j, i - 2, j + 2) &&
+                                IsJump(gameBoard, player.Color, i, j, i - 2, j + 2))
+                            {
+                                o_allJumpAvailable.Add(new ArrayList() { i - 2, j + 2 });
+
+                            }
+                        }
+                        if (isKing || (gameBoard.Board[i, j].CoinColor.CompareTo('O') == 0))
+                        {
+                            if (MoveIsInbound(gameBoard, i, j, i + 2, j - 2) &&
+                                IsJump(gameBoard, player.Color, i, j, i + 2, j - 2))
+                            {
+                                o_allJumpAvailable.Add(new ArrayList() { i + 2, j - 2 });
+                            }
+                            if (MoveIsInbound(gameBoard, i, j, i + 2, j + 2) &&
+                                IsJump(gameBoard, player.Color, i, j, i + 2, j + 2))
+                            {
+                                o_allJumpAvailable.Add(new ArrayList() { i + 2, j + 2 });
+
+                            }
+                        }
+
+                    }
+                }
+            }
+            return o_allJumpAvailable;
+        }
+
+        private static ArrayList allSimpleMoveAvailable(GameBoard gameBoard, Player player)
+        {
+            ArrayList o_allSimpleMoveAvailable = new ArrayList();
+
+            for (int i = 1; i < gameBoard.BoardSize - 1; i++)
+            {
+                for (int j = 1; j < gameBoard.BoardSize - 1; j++)
+                {
+                    if (tileOccupiedByColor(gameBoard, i, j, player.Color))
+                    {
+                        bool isKing = gameBoard.Board[i, j].IsKing;
+
+                        if (isKing || (gameBoard.Board[i, j].CoinColor.CompareTo('X') == 0))
+                        {
+                            if (MoveIsInbound(gameBoard, i, j, i - 1, j - 1) &&
+                                IsSimpleMove(gameBoard,player.Color, i, j, i - 1, j - 1))
+                            {
+                                o_allSimpleMoveAvailable.Add(new ArrayList() { i - 1, j - 1 });
+                            }
+                            if (MoveIsInbound(gameBoard, i, j, i - 1, j + 1) &&
+                              IsSimpleMove(gameBoard, player.Color, i, j, i - 1, j + 1))
+                            {
+                                o_allSimpleMoveAvailable.Add(new ArrayList() { i - 1, j + 1 });
+
+                            }
+                        }
+                        if (isKing || (gameBoard.Board[i, j].CoinColor.CompareTo('O') == 0))
+                        {
+                            if (MoveIsInbound(gameBoard, i, j, i + 1, j - 1) &&
+                                IsSimpleMove(gameBoard, player.Color, i, j, i + 1, j - 1))
+                            {
+                                o_allSimpleMoveAvailable.Add(new ArrayList() { i + 1, j - 1 });
+                            }
+                            if (MoveIsInbound(gameBoard, i, j, i + 1, j + 1) &&
+                                IsSimpleMove(gameBoard, player.Color, i, j, i + 1, j + 1))
+                            {
+                                o_allSimpleMoveAvailable.Add(new ArrayList() { i + 1, j + 1 });
+
+                            }
+                        }
+
+                    }
+                }
+            }
+            return o_allSimpleMoveAvailable;
+        }
+
+        
     }
 }
 
